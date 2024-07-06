9f6053f6b9db09af5aec7a9de83e9595c90e4eba
pranav007082 <148082845+pranav007082@users.noreply.github.com>
2024-07-06 09:12:46 +0000

    container done

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,10 @@
 function App() {
 
   return (
+    <> {/* this is called segment ,like this we can wrap multiple html elemetns */}
     <h1>Pranav with vite</h1>
+    <h2>Coffe aur Chai</h2>
+    </>
   )
 }
 
