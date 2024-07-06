664fc9c5203a37e5bd7e70d5e8bfea64c7faca2a
pranav007082 <148082845+pranav007082@users.noreply.github.com>
2024-07-06 19:16:00 +0000

    cont done

--- 01vitereact/src/hooks.jsx
+++ 01vitereact/src/hooks.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { useState } from 'react';//hooks
+import './index.css';
 
 function Changecolor(){
-    let [color,setcolor] = useState("blue")
-    return(<>
-        <h1 >My Favourite color is {color}</h1>
-        <button onClick={() => setcolor("green")}>Green</button>
-        <button onClick={() => setcolor("red")}>Red</button>
-        <button onClick={() => setcolor("blue")}>Blue</button>
-        <button onClick={() => setcolor("violet")}>violet</button>
-        </>
+    let [color,setColor] = useState("olive")
+    return(
+        <div className='w-full h-screen flex flex-col items-center justify-center duration-200' style={{ backgroundColor: color }}>
+            <h1 className='mb-4 text-2xl'>My Favourite color is {color}</h1>
+            <div className='space-x-4'>
+                <button className='px-4 py-2 bg-green-500 text-white rounded' onClick={() => setColor("green")}>Green</button>
+                <button className='px-4 py-2 bg-red-500 text-white rounded' onClick={() => setColor("red")}>Red</button>
+                <button className='px-4 py-2 bg-blue-500 text-white rounded' onClick={() => setColor("blue")}>Blue</button>
+                <button className='px-4 py-2 bg-yellow-500 text-white rounded' onClick={() => setColor("yellow")}>Yellow</button>
+            </div>
+        </div>
     )
 }
 
