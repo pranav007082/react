0213334e761ceb339558b0a6eacaf083ec9ecea2
pranav007082 <148082845+pranav007082@users.noreply.github.com>
2024-07-06 14:23:35 +0000

    cont done

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <> {/* this is called segment ,like this we can wrap multiple html elemetns */}
-    <h1>Pranav with vite</h1>
+    <h1 className="bg-green-400 text-white">Pranav with vite</h1>
     <h2>Coffe aur Chai</h2>
     </>
   )
--- 01vitereact/src/hooks.jsx
+++ 01vitereact/src/hooks.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { useState } from 'react';
+import { useState } from 'react';//hooks
 
 function Changecolor(){
     let [color,setcolor] = useState("blue")
     return(<>
-        <h1>My Favourite color is {color}</h1>
+        <h1 >My Favourite color is {color}</h1>
         <button onClick={() => setcolor("green")}>Green</button>
         <button onClick={() => setcolor("red")}>Red</button>
         <button onClick={() => setcolor("blue")}>Blue</button>
